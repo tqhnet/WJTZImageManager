165f295bb10e4add9fceee94de318c37616fbc72
tqhnet <tqhnet@163.com>
2017-12-08 00:53:37 +0800

    上传

---
+++ TZImagePickerController/other/TZProgressView.h
@@ -0,0 +1,16 @@
+//
+//  TZProgressView.h
+//  TZImagePickerController
+//
+//  Created by ttouch on 2016/12/6.
+//  Copyright © 2016年 谭真. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**进度视图*/
+@interface TZProgressView : UIView
+
+@property (nonatomic, assign) double progress;
+
+@end
---
+++ TZImagePickerController/video/TZVideoPlayerController.h
@@ -0,0 +1,18 @@
+//
+//  TZVideoPlayerController.h
+//  TZImagePickerController
+//
+//  Created by 谭真 on 16/1/5.
+//  Copyright © 2016年 谭真. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TZAssetModel;
+
+/**视频预览*/
+@interface TZVideoPlayerController : UIViewController
+
+@property (nonatomic, strong) TZAssetModel *model;
+
+@end
